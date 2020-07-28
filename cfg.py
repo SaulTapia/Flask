c4718637bb2f12e30bb0f1a763f891d112fa7f4a
Saul Tapia <19080045@cobachih.edu.mx>
2020-07-28 14:29:14 -0600

    Forgot to change 'app' to 'app_assets' in cfg.py

--- cfg.py
+++ cfg.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 some_engine = create_engine('postgres://oeuvggnnhwwple:28b51e993089eff942c53fb403a780543a8\
 ef6d2887c829371c46065659a5c83@ec2-50-16-198-4.compute-1.amazonaws.com:5432/d40jvmpmbjumr')
 sqlsessionclass = sessionmaker(bind=some_engine)
-from app import create_app
+from app_assets import create_app
 app = create_app()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://oeuvggnnhwwple:28b51e993089eff942c53fb403a780\
 543a8ef6d2887c829371c46065659a5c83@ec2-50-16-198-4.compute-1.amazonaws.com:5432/d40jvmpmbjumr'
